d4c842e933456b23e8f27673195a00df48211656
devcabezas <luiscabezas25@hotmail.com>
2021-07-15 08:48:28 -0500

    Maquetacion de centro de la Card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,24 @@ import Button from './Button';
 const App = () => {
   return (
     <>
-      <h1>SPLI <br/> TTER</h1>
-      <Input />
-      <Button/>
+        <h1>SPLI <br/> TTER</h1>
+      <div className="container">
+        <div className="container__content">
+          <div className="container__content--left">
+            <Input />
+            <Button/>
+            <Input />
+          </div>
+          <div className="container__content--right">
+            <div>
+              <p>
+                Tip Amount
+              </p>
+              <span>/ person</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
