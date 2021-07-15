2a660926d7166409c485e82311b03fc4a1f4ab9c
devcabezas <luiscabezas25@hotmail.com>
2021-07-15 08:07:50 -0500

    Boton y Estilos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Input from './Input';
+import Button from './Button';
 
 const App = () => {
   return (
     <>
       <h1>SPLI <br/> TTER</h1>
       <Input />
+      <Button/>
     </>
   )
 }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button className="btn">
+      5%
+    </button>
+  )
+}
+
+export default Button
