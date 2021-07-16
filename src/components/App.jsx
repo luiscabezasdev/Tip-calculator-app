a01d87b7a038c130367b1c4b853d5dbe0bdd78d0
devcabezas <luiscabezas25@hotmail.com>
2021-07-16 09:36:59 -0500

    Estilos Layaut

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,34 @@ import Button from './Button';
 const App = () => {
   return (
     <>
-        <h1>SPLI <br/> TTER</h1>
+      <h1>SPLI <br/> TTER</h1>
       <div className="container">
         <div className="container__content">
           <div className="container__content--left">
             <Input />
-            <Button/>
+            <p className="container__text">Select Tip %</p>
+            <div className="container__btn">
+              <Button/>
+              <Button/>
+              <Button/>
+              <Button/>
+              <Button/>
+              <Button/>
+            </div>
             <Input />
           </div>
           <div className="container__content--right">
-            <div>
+            <div className="container__content--right-text">
               <p>
                 Tip Amount
               </p>
               <span>/ person</span>
+              <p>
+                Total
+              </p>
+              <span>/ person</span>
             </div>
+            <Button/>
           </div>
         </div>
       </div>
